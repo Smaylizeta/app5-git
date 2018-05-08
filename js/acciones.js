df31053ebd9bda505e6e6331ceb55b354423c399
alberto velasco <cb16.velasco.bautista.alberto@gmail.com>
2018-05-08 14:48:52 -0500

    1subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,12 +1,12 @@
 // JavaScript Document
 $(document).ready(function(e){
-   document.addEventListener("deviceready", function(){
-	   $('#sonar').tap(function(){
+   document.addEventListener("deviceready",function(){
+	   $('#beep').tap(function(){
 		   navigator.notification.beep(2);
 	  });  //cierre el beep
-	  $('vibrar').tap(function(){
-		  navigator.notification.vibrate(2000);
-	  }); //cierre del vibrar
+	  $('#vibrar').tap(function(){
+		  navigator.notification.vibrate(2000)
+	  });//cierre del vibrar
    },false); //cierre del deviceready
 }); //cierre ready
 
